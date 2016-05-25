961de4c03fdda7da7da4eabf32a696522f477d45
dbarelop <dbarelop@gmail.com>
2016-05-25 09:29:32 +0200

    Worked on insertcore's functionality

--- Aufgabe3/VHDL/insertcore.vhd
+++ Aufgabe3/VHDL/insertcore.vhd
@@ -23,4 +23,65 @@ END insertcore;
 
 ARCHITECTURE verhalten OF insertcore IS
 
+    CONSTANT MAX_LEN: positive := 256;
+
+    -- Sorting function (reference)
+    PROCEDURE sort(a: INOUT string; n: positive) IS
+        VARIABLE key: character;
+        VARIABLE j: integer;
+    BEGIN
+        FOR i IN 1 TO n - 1 LOOP        -- S0
+            key := a(i);                -- S1
+            j := i - 1;
+            WHILE j >= 0 LOOP
+                IF a(j) <= key THEN     -- S2
+                    EXIT;
+                END IF;
+                a(j + 1) := a(j);       -- S3
+                j := j - 1;
+            END LOOP;
+            a(j + 1) := key;            -- S4
+        END LOOP;
+    END PROCEDURE;
+    
+    TYPE tstate IS (IDLE, S0, S1, S2, S3, S4);
+    SIGNAL state: tstate := IDLE;
+    SIGNAL key: character;
+    SIGNAL i: integer;
+    SIGNAL j: integer;
+    SIGNAL data_j: character;
+
+BEGIN
+
+    PROCESS (rst, clk) IS
+    BEGIN
+        IF rst = RSTDEF OR (rising_edge(clk) AND strt = '1') THEN
+            IF strt = '1' THEN
+                state := S0;
+            ELSE
+                state := IDLE;
+            END IF;
+            done <= '0';
+            WEB  <= '0';
+            ENB  <= '0';
+            i    <=  0;
+            j    <=  0;
+        ELSIF rising_edge(clk) THEN
+            CASE state IS
+                WHEN IDLE =>
+                WHEN S0 =>
+                    -- Load a(i) from RAM
+                    WEB <= '0';
+                    ENB <= '1';
+                    ADR <= ...;
+                WHEN S1 =>
+                    -- key := a(i)
+                    
+                WHEN S2 =>
+                WHEN S3 =>
+                WHEN S4 =>
+            END CASE;
+        END IF;
+    END PROCESS;
+
 END verhalten;
